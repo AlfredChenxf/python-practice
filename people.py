e0cfaf8e937d2a571a133126b7e56cc07d1e99fd
AlfredChenxf <alexalfred@163.com>
2015-03-24 16:25:04 +0800

    test

---
+++ people.py
@@ -0,0 +1,10 @@
+__author__ = 'Alfred'
+
+class people(object):
+    def __init__(self):
+        print "Init class"
+
+    def getName(self, name):
+        print "getName in class people: ", name
+        return name
+
